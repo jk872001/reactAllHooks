ea4e67378b17654b418593af233a6f875f78f6fd
jk872001 <jiteshkumar872001@gmail.com>
2023-01-03 16:31:46 +0530

    useMemo hook explained

---
+++ reacthooks/src/App.js
@@ -0,0 +1,10 @@
+import UseMemoHook from "./hooks/useMemoHook";
+function App() {
+  return (
+    <div className="App">
+      <UseMemoHook />
+    </div>
+  );
+}
+
+export default App;
---
+++ reacthooks/src/hooks/useMemoHook.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState, useMemo } from "react";
+ const UseMemoHook = () => {
+  const [myNum, setMyNum] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const getValue = () => {
+    return setMyNum(myNum + 1);
+  };
+
+  const countNumber = (num) => {
+    console.log("🚀 ~ file: Memo.jsx ~ line 12 ~ countNumber ~ num", num);
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num;
+  };
+
+  // subscribe to thapa technical
+
+  const checkData = useMemo(() => {
+    return countNumber(myNum);
+  }, [myNum]);
+
+  return (
+    <div>
+    <button onClick={getValue} style={{ backgroundColor: "red" }}>
+    Counter
+  </button>
+  <p> My new number : {checkData} </p>
+  <button onClick={() => setShow(!show)}>
+    {show ? "You clicked me" : "Click me plz"}
+  </button>
+
+    </div>
+  )
+}
+export default UseMemoHook
